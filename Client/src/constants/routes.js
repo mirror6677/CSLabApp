b9d5ceafbb9e15fbf6134aa7f0117ff9eec6a994
Alan Marchiori <labapp@linuxremote3.bucknell.edu>
2018-12-12 11:15:53 -0500

    correctly named API_ROOT

--- Client/src/constants/routes.js
+++ Client/src/constants/routes.js
@@ -1,2 +1,11 @@
-export const API_ROOT = 'http://localhost:8000'
+
+//export const API_ROOT = 'http://localhost:8000'
+export let API_ROOT;
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    API_ROOT = 'http://localhost:8000'
+} else {
+    // production code
+    API_ROOT = 'https://www.eg.bucknell.edu/labapp'
+}
+
 export const BUCKNELL_API = 'https://myapi.bucknell.edu/framework'
